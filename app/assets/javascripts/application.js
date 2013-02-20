ccbc40827c87047e61c8f55c4c696b2c7d460902
penguinlover <qjavaswing2009@gmail.com>
2013-02-20 15:53:16 -0600

    What a messy asset pipeline require horror

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+// require bootstrap
 //
 //= require underscore
 // require json2
@@ -25,9 +25,9 @@
 //
 //= require timesheet_app
 //
-//= require_tree ./models
-//= require_tree ./collections 
-//= require_tree ./views 
-//= require_tree ./routers
-//= require_tree ../templates
+// require_tree ./models
+// require_tree ./collections 
+// require_tree ./views 
+// require_tree ./routers
+// require_tree ../templates
 //= require_tree .
